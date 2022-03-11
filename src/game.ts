1819d344e23a6a6b5d2e4f15f7ea793ad84fbd5c
ization111 <101299428+ization111@users.noreply.github.com>
2022-03-11 14:45:30 -0600

    Add files via upload

---
+++ src/game.ts
@@ -0,0 +1,24 @@
+
+const _scene = new Entity('_scene')
+engine.addEntity(_scene)
+const transform = new Transform({
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+_scene.addComponentOrReplace(transform)
+
+const entity = new Entity('entity')
+engine.addEntity(entity)
+entity.setParent(_scene)
+const gltfShape = new GLTFShape("9be6610f-cf2b-47fc-a5bd-c9aa62ea6f8e/FloorFantasyRocks_01/FloorFantasyRocks_01.glb")
+gltfShape.withCollisions = true
+gltfShape.isPointerBlocker = true
+gltfShape.visible = true
+entity.addComponentOrReplace(gltfShape)
+const transform2 = new Transform({
+  position: new Vector3(8, 0, 8),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1)
+})
+entity.addComponentOrReplace(transform2)
